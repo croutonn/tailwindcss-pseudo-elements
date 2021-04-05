f07a2a230a2b4ce1cd7b9c72ed1806382e51d2bd
croutonn <croutonna@gmail.com>
2021-04-05 22:53:59 +0900

    fix: Fix the warnings in ESLint

--- index.js
+++ index.js
@@ -1,67 +1,68 @@
+const tailwindcssPlugin = require('tailwindcss/plugin')
+
 const addContentUtilities = require('./content-utilities')
 const { pseudoElements, pseudoClasses } = require('./lib')
 
 const plugin = (options = {}) => {
   const { contentUtilities = true } = options
 
-  return require('tailwindcss/plugin')(
-    ({ addUtilities, addVariant, e, config }) => {
-      const customPseudoElements = config
-        ? config('customPseudoElements') || []
-        : []
-      const customPseudoClasses = config
-        ? config('customPseudoClasses') || []
-        : []
+  return tailwindcssPlugin(({ addUtilities, addVariant, e, config }) => {
+    const customPseudoElements = config
+      ? config('customPseudoElements') || []
+      : []
+    const customPseudoClasses = config
+      ? config('customPseudoClasses') || []
+      : []
 
-      if (!Array.isArray(customPseudoElements)) {
-        throw new Error('`customElements` must be an array of string.')
-      }
+    if (!Array.isArray(customPseudoElements)) {
+      throw new Error('`customElements` must be an array of string.')
+    }
 
-      if (!Array.isArray(customPseudoClasses)) {
-        throw new Error('`customClasses` must be an array of string.')
-      }
+    if (!Array.isArray(customPseudoClasses)) {
+      throw new Error('`customClasses` must be an array of string.')
+    }
 
-      const mergedPseudoElements = Array.from(
-        new Set(pseudoElements.concat(customPseudoElements))
-      )
-      const mergedPseudoClasses = Array.from(
-        new Set(pseudoClasses.concat(customPseudoClasses))
-      )
+    const mergedPseudoElements = Array.from(
+      new Set(pseudoElements.concat(customPseudoElements))
+    )
+    const mergedPseudoClasses = Array.from(
+      new Set(pseudoClasses.concat(customPseudoClasses))
+    )
 
+    mergedPseudoElements.forEach((pelement) => {
+      addVariant(pelement, ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`${pelement}${separator}${className}`)}::${pelement}`
+        })
+      })
+    })
+
+    mergedPseudoClasses.forEach((pclass) => {
       mergedPseudoElements.forEach((pelement) => {
-        addVariant(pelement, ({ modifySelectors, separator }) => {
-          modifySelectors(({ className }) => {
-            return `.${e(`${pelement}${separator}${className}`)}::${pelement}`
-          })
-        })
+        addVariant(
+          `${pclass}_${pelement}`,
+          ({ modifySelectors, separator }) => {
+            modifySelectors(({ className }) => {
+              return `.${e(
+                `${pclass}${separator}${pelement}${separator}${className}`
+              )}:${pclass}::${pelement}`
+            })
+          }
+        )
       })
+    })
 
-      mergedPseudoClasses.forEach((pclass) => {
-        mergedPseudoElements.forEach((pelement) => {
-          addVariant(
-            `${pclass}_${pelement}`,
-            ({ modifySelectors, separator }) => {
-              modifySelectors(({ className }) => {
-                return `.${e(
-                  `${pclass}${separator}${pelement}${separator}${className}`
-                )}:${pclass}::${pelement}`
-              })
-            }
-          )
-        })
-      })
-
-      if (contentUtilities) {
-        addContentUtilities({
-          addUtilities,
-          prefix: contentUtilities.hasOwnProperty('prefix')
+    if (contentUtilities) {
+      addContentUtilities({
+        addUtilities,
+        prefix:
+          'prefix' in contentUtilities
             ? contentUtilities.prefix || 'tw-content'
             : 'tw-content',
-          pseudoClasses: mergedPseudoClasses,
-        })
-      }
+        pseudoClasses: mergedPseudoClasses,
+      })
     }
-  )
+  })
 }
 
 module.exports = plugin
