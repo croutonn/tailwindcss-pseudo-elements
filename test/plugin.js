48953467cbbad71422954884e8dbeb7735f9b9d5
croutonn <croutonna@gmail.com>
2021-04-05 22:37:28 +0900

    test: Change to create a postcss processor for each test

--- test/plugin.js
+++ test/plugin.js
@@ -2,12 +2,19 @@ const { assert } = require('chai')
 const postcss = require('postcss')
 const tailwindcss = require('tailwindcss')
 
-const processor = postcss([tailwindcss('./test/tailwind.config.js')])
+const baseConfig = require('./tailwind.config')
+
+const createProcessor = (config = {}) => {
+  return postcss([tailwindcss({ ...baseConfig, ...config })])
+}
 
 describe('Basic Usage', () => {
   it('@tailwind', async () => {
     const testData = `@tailwind utilities;`
-    const { css } = await processor.process(testData, { from: '', to: '' })
+    const { css } = await createProcessor().process(testData, {
+      from: '',
+      to: '',
+    })
     assert.include(css, '.before\\:text-black::before')
     assert.include(
       css,
@@ -17,7 +24,10 @@ describe('Basic Usage', () => {
 
   it('@variants', async () => {
     const testData = `@variants after,before{.text-blue{color:blue}}`
-    const { css } = await processor.process(testData, { from: '', to: '' })
+    const { css } = await createProcessor().process(testData, {
+      from: '',
+      to: '',
+    })
     assert.include(css, '.after\\:text-blue::after')
     assert.include(css, '.before\\:text-blue::before')
   })
@@ -26,13 +36,19 @@ describe('Basic Usage', () => {
 describe('with pseudo-classes', () => {
   it('@tailwind', async () => {
     const testData = `@tailwind utilities;`
-    const { css } = await processor.process(testData, { from: '', to: '' })
+    const { css } = await createProcessor().process(testData, {
+      from: '',
+      to: '',
+    })
     assert.include(css, '.hover\\:before\\:text-black:hover::before')
   })
 
   it('@variants', async () => {
     const testData = `@variants hover_after,hover_before{.text-blue{color:blue}}`
-    const { css } = await processor.process(testData, { from: '', to: '' })
+    const { css } = await createProcessor().process(testData, {
+      from: '',
+      to: '',
+    })
     assert.include(css, '.hover\\:after\\:text-blue:hover::after')
     assert.include(css, '.hover\\:before\\:text-blue:hover::before')
   })
@@ -41,13 +57,19 @@ describe('with pseudo-classes', () => {
 describe('with configuration', () => {
   it('@tailwind', async () => {
     const testData = `@tailwind utilities;`
-    const { css } = await processor.process(testData, { from: '', to: '' })
+    const { css } = await createProcessor().process(testData, {
+      from: '',
+      to: '',
+    })
     assert.include(css, '.foo\\:bar\\:text-black:foo::bar')
   })
 
   it('@variants', async () => {
     const testData = `@variants foo_bar{.text-blue{color:blue}}`
-    const { css } = await processor.process(testData, { from: '', to: '' })
+    const { css } = await createProcessor().process(testData, {
+      from: '',
+      to: '',
+    })
     assert.include(css, '.foo\\:bar\\:text-blue:foo::bar')
   })
 })
